65e105b6bb6422560379084a2d3da0867800ec1a
Alex-byte256 <oleksandr.kozak.2019@gmail.com>
2022-12-15 17:38:59 +0300

    add functional

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,12 @@
 export default function fetchCountries(name) {
-  fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,captail`)
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,captail,population,flags,languages`
+  )
     .then(res => res.json())
-    .then(console.log)
+    .then(countries => {
+      console.log(countries);
+      return countries;
+    })
     .catch(console.log)
     .finally(() => console.log('запит звершено'));
 }
